6db98a48651d2c2f4ee1c9d6af2eba3b8fd93950
Mark Mozarovski <mark.mozh@gmail.com>
2025-01-12 13:50:49 +0100

    fix main menu

--- ui/camera_2d.gd
+++ ui/camera_2d.gd
@@ -2,5 +2,8 @@ extends Camera2D
 
 @onready var player: CharacterBody2D = %Player
 
+func _ready() -> void:
+	global_position = player.global_position
+
 func _process(_delta: float) -> void:
 	global_position = (player.global_position * 3 + get_global_mouse_position()) / 4
--- ui/main_menu_camera.gd
+++
@@ -1,5 +0,0 @@
-extends Camera2D
-
-
-func _process(_delta: float) -> void:
-	global_position = (get_viewport_rect().size / 2 * 5 + get_global_mouse_position()) / 6
