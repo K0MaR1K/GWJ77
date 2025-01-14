2727df14d91ce5bb8a5b097fb1d02d5509f8bbf0
Mark Mozarovski <mark.mozh@gmail.com>
2025-01-14 12:05:41 +0100

    add phase manager

--- globals/phase_manager.gd
+++ globals/phase_manager.gd
@@ -1,5 +1,40 @@
 extends Node
 
-enum Phase {INTRO, GAME1, ROOM1, GAME2, ROOM2}
+enum Phase {INTRO, ROOM1, ROOM2, GAME2, ROOM3}
+enum LDJ {HAPPY, SAD, MYSTERIOUS}
 
-var 
+var current_phase : Phase = Phase.INTRO
+
+var computer_on := false
+var can_leave_computer := false
+var plate := false
+var level1 := false
+var level2 := false
+var level3 := false
+var loading_jingle := LDJ.HAPPY
+
+func next_phase():
+	change_phase(current_phase + 1)
+
+func change_phase(next_phase: Phase):
+	current_phase = next_phase
+	match current_phase:
+		Phase.INTRO:
+			computer_on = false
+			can_leave_computer = false
+			plate = false
+			level1 = true
+			level2 = true
+			level3 = false
+			loading_jingle = LDJ.HAPPY
+		Phase.ROOM1:
+			computer_on = false
+			can_leave_computer = true
+			plate = false
+			level1 = true
+			level2 = true
+			level3 = false
+		Phase.ROOM2:
+			pass
+		Phase.ROOM2:
+			pass
